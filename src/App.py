f1af0e1ae680c00fcfb804e398c39e276529429b
nbaak <niklas.baak@outlook.com>
2023-01-11 14:50:34 +0100

    refactoring

--- src/App.py
+++ src/App.py
@@ -7,27 +7,28 @@ import argparse
 
 from Peers import Peers
 from datetime import datetime
+from asyncio.streams import StreamReader, StreamWriter
 
 
-async def server(_reader, _writer):
+async def server(reader:StreamReader, writer:StreamWriter):
     try:
         while True:
-            peer = ':'.join(map(str, _reader._transport.get_extra_info('peername')))
+            peer = ':'.join(map(str, reader._transport.get_extra_info('peername')))
             if Peers.add_peer(peer):
                 connection_time = Peers.connections[peer]
                 timestamp = connection_time.strftime("%Y-%m-%d %H:%M:%S")
                 logging.info(f'[{timestamp}] ACCEPT host={peer}')
 
-            _writer.write(b'%x\r\n' % random.randint(0, 2 ** 32))
+            writer.write(b'%x\r\n' % random.randint(0, 2 ** 32))
 
             await asyncio.sleep(3)
-            await _writer.drain()
+            await writer.drain()
 
     except BrokenPipeError:
         connection_duration = Peers.get_connection_duration(peer)
-        
-        Peers.remove_peer(peer) # remove peer
-        
+
+        Peers.remove_peer(peer)  # remove peer
+
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         logging.info(f'[{timestamp}] CLOSE host={peer} time={connection_duration.total_seconds()}')
 
@@ -45,11 +46,11 @@ def main():
     logging.basicConfig(format='%(message)s',
                         encoding='utf-8',
                         level=logging.INFO)
-    
+
     parser = argparse.ArgumentParser(description="SSH Tarpit")
     parser.add_argument('--port', '-p', help="Set Port for the Tarpit (default 22222)", default=22222, type=int, action="store")
     args = parser.parse_args()
-    
+
     asyncio.run(start_server(args.port))
 
 
