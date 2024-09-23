698165bcf31272bafce4f54839f2be1868a948f9
nbaak <niklas@baak.xyz>
2024-09-23 20:45:46 +0200

    fixed geoip adapter

--- src/geoip_service.py
+++ src/geoip_service.py
@@ -7,11 +7,30 @@ import settings
 def get_location_data(ip:str):
 
     if settings.geoip_service:
-        geoip_data = requests.get(settings.geoip_service + '/' + str(ip)).json()
-        country_code = geoip_data['code'] if not '-' else 'unkown'
-        country = geoip_data['country'] if not '-' else 'unkown'
+        geoip_data = requests.get(f"{settings.geoip_service}/{str(ip)}").json()
+        country_code = geoip_data['code']
+        country = geoip_data['country']
+        
+        if country_code == "-":
+            country_code = "unknown"
+        if country == "-":
+            country = "unknown"            
     else:
         country = 'unknown'
         country_code = 'unknown'
 
     return country_code, country
+
+
+
+def test():
+    ip = "92.206.197.156"
+    print(get_location_data(ip))
+    
+    ip = "127.0.0.1"
+    print(get_location_data(ip))
+
+
+if __name__ == "__main__":
+    test()
+    
